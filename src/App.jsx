d6c1fb7bf26412073c8a1742e0c0608b8fbd3088
MoniLopez05 <Monicalo94008118@>
2023-09-03 18:49:21 -0300

    se modifico

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css'
 import Form from './Form';
 import Card from './Card';
 
@@ -6,15 +7,17 @@ function App() {
   const [musicas, setMusica] = useState([]);
   const agregarMusica = (musica) => {
     setMusica([...musicas,musica]);
-
+  }
   return (
-    <div className = 'app'>
+    <>
+    <div className='app'>
       <h1> Mi Musica Favorita</h1>
       <Form onAgregarMusica ={agregarMusica}></Form>
       <Card musicas = {musicas}></Card>
-      
+     
     </div>
+    </> 
   )
-}
+
 }
 export default App
